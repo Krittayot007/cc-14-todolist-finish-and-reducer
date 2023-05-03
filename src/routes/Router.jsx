7a04543a7ee3d3b6bed994d942999f677a950f75
Krittayot007 <krittayot.som@gmail.com>
2023-05-03 17:30:35 +0700

    config:publicRoute

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,27 +1,36 @@
-import { createBrowserRouter,RouterProvider } from 'react-router-dom' 
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom' 
+import { publicRoutes } from './publicRoutes'
 
 function Router() {
-    
-    const router = createBrowserRouter ([
-        {
-            path: '/',
-            element: <h1>Home</h1>
-        },
-        {
-            path: 'profile',
-            element: <h1>profile</h1>
-        },
-        {
-            path: 'profile/:id',
-            element: <h1>profile</h1>
-        },
-        {
-            path: 'about',
-            element: <h1>about</h1>
-        }
-    ])
 
+
+    const router = createBrowserRouter(publicRoutes)
   return <RouterProvider router={router} />
 }
 
-export default Router
+export default Router 
+
+// Ex : Array of Router Object
+// const mockedRotes = [
+//         {
+//             path: '/',
+//             element: <h1>Home</h1>
+//         },
+//         {
+//             path: 'profile',
+//             element: <h1>profile</h1>
+//         },
+//         {
+//             path: 'profile/:id',
+//             element: <h1>profile</h1>
+//         },
+//         {
+//             path: 'about',
+//             element: <h1>about</h1>
+//         },
+//         {
+//             path: '*',
+//             element: <Navigate to='/' />
+//         }
+//     ]
+
